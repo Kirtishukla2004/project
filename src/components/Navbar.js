06a5440bc89b5141b39d4581bd653e80580148a9
kirti <shuklakirti2004@gmail.com>
2023-09-21 19:40:00 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import './navbar.css';
-import logoimage from './image/logo-techno.png'; 
+import React from "react";
+import "./navbar.css";
+
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div className='header'>
+      <div className="header">
         <nav className="navbar">
           <div className="logo">
-            <img src="image/logo-techno.png" alt="Techbo Logo" />
+            <img src="/logo-techno.png" alt="Techbo Logo" />
           </div>
           <ul className="nav-links">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Services</a></li>
-            <li><a href="/contact">Samples</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Services</a>
+            </li>
+            <li>
+              <a href="/contact">Samples</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
           </ul>
         </nav>
       </div>
